f2d0763391950ac4c8975b85b1b40437fc22e899
jh.cho <ctp102>
2022-11-29 11:36:59 +0900

    initial commit

---
+++ src/main/java/com/ctp102/module/oauth/OauthModuleApplication.java
@@ -0,0 +1,13 @@
+package com.ctp102.module.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ctp102/module/oauth/OauthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctp102.module.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
